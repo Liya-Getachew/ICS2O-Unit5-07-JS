7040d33478422727c7fcbc5a83342ec9a8549f71
Liya-Getachew <113055994+Liya-Getachew@users.noreply.github.com>
2022-12-05 20:06:14 -0500

    pwa

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 // Copyright (c) 2022 liya getachew All rights reserved
 //
 // Created by: liya getachew
-// Created on: Oct 2022
+// Created on: Dec 2022
 // This file contains the JS functions for index.html
 
 "use strict"
@@ -10,13 +10,13 @@
  * Check servie worker.
  */
 if (navigator.serviceWorker) {
-  navigator.serviceWorker.register("/ICS2O-PWA-Test/sw.js", {
-    scope: "/ICS2O-PWA-Test/",
+  navigator.serviceWorker.register("/ICS2O-Unit5-07-JS/sw.js", {
+    scope: "/ICS2O-Unit5-07-JS/",
   })
 }
 
 /**
- * This function displays an alert.
+ * This function counts up from 1 to the user's chosen value.
  */
 function myButtonClicked() {
   document.getElementById("hello-world").innerHTML = "<p>Hello, World!</p>"
